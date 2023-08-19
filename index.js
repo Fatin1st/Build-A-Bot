cd78e37c90296a5d29e4b30b22387f611b88539a
unknown <fatinhasnat.27sep2000@gmail.com>
2023-08-19 16:46:37 +0600

    final changes commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to BuildABot");
 });
 
-app.get("/chat/:apiKey/:behaviour/:message", async (req, res) => {
+app.get("/bot/:apiKey/:behaviour/:message", async (req, res) => {
   const userMessage = req.params.message;
   const behaviour = req.params.behaviour;
   const apiKey = req.params.apiKey;
